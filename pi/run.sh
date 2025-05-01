a54f601785d67fc718f57878b5df23d701bf2342
waxz <waxz@github.com>
2025-05-01 14:05:30 +0000

    add pi-hole

--- pi/run.sh
+++ pi/run.sh
@@ -1,3 +1,5 @@
 sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
+echo DNS=127.0.0.1 | sudo tee -a  /etc/systemd/resolved.conf
+sudo systemctl restart systemd-resolved
