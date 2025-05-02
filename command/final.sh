1632f6c8e41fbb9f78d92793e3a489837c80ce33
waxz <waxz@github.com>
2025-05-02 06:44:26 +0000

    add git temp commit

--- command/final.sh
+++ command/final.sh
@@ -16,6 +16,14 @@ cd /tmp/action_test && gh run list --workflow=run_proxy.yml
 cd /tmp/action_test && gh workflow run .github/workflows/run_proxy.yml && gh run list --workflow=run_proxy.yml
 
 
+# save change 
+# https://stackoverflow.com/questions/7124726/git-add-only-modified-changes-and-ignore-untracked-files
+# https://stackoverflow.com/questions/2944469/how-to-commit-my-current-changes-to-a-different-branch-in-git
+SAVE='git add -u && git checkout -b temp-save && git commit -m "WIP: save before shutdown" && git push origin temp-save'
+RESTORE=`git checkout main && git merge temp-save`
+
+
+
 echo "commit github update"
 (cd /tmp/self-host-reader/ && git add --all && git commit -m "update" && git push origin main ) || true
 (cd /tmp/quartz/ && git add --all && git commit -m "update" && git push origin main ) || true
---
+++ command/git-temp-commit.sh
@@ -0,0 +1,4 @@
+git add -u 
+git checkout -b temp-save
+git commit -m "WIP: save before shutdown"
+git push origin temp-save
