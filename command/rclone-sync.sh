7ed3c75f1e647af556c62d6a1860217bb9a59f1a
waxz <waxz@github.com>
2025-05-11 14:08:48 +0000

    add rclone permmision flag, not work sync from remote

--- command/rclone-sync.sh
+++ command/rclone-sync.sh
@@ -19,13 +19,13 @@ LOG="/tmp/rclone-sync.log"
 
 if [ ! -z "$D" ];then
     echo  download
-    rclone sync  "$REMOTE" "$WATCH_DIR" --filter-from "$FILTER" -v
+    rclone sync  "$REMOTE" "$WATCH_DIR"  -M --filter-from "$FILTER" -v
 
     exit 0;
 fi;
 if [ ! -z "$U" ];then
     echo upload
-    rclone sync  "$WATCH_DIR"  "$REMOTE" --filter-from "$FILTER" -v
+    rclone sync  "$WATCH_DIR"  "$REMOTE"  -M --filter-from "$FILTER" -v
     exit 0;
 fi;
 
@@ -33,7 +33,7 @@ fi;
 if [ ! -z "$S" ];then
     while inotifywait -r -e modify,create,delete,move "$WATCH_DIR"; do
         echo "$(date): Change detected, syncing..." >> "$LOG"
-        rclone sync "$WATCH_DIR" "$REMOTE" --filter-from "$FILTER" >> "$LOG" 2>&1
+        rclone sync "$WATCH_DIR" "$REMOTE"   -M  --filter-from "$FILTER" >> "$LOG" 2>&1
     done
 fi;
 
