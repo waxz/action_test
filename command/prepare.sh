7ecdd798274f99dce7e51861753085f6eb8c98e0
waxz <waxz@github.com>
2025-05-14 13:16:46 +0000

    change command order

--- command/prepare.sh
+++ command/prepare.sh
@@ -33,3 +33,32 @@ sudo chmod 600 /mnt/swapfile
 sudo mkswap  /mnt/swapfile
 sudo swapon /mnt/swapfile
 
+
+cat << 'EOF' | tee -a $HOME/.bashrc
+
+## set modules-folder for yarn
+myarn(){
+
+  echo "Number of arguments: $#"
+  echo "All arguments as separate words: $@"
+  echo "All arguments as a single string: $*"
+
+  for arg in "$@"; do
+    echo "Argument: $arg"
+  done
+#echo "Original list: $@"
+#shift
+#echo "List after removing the first element: $@"
+
+ if [ ! -z "$CUSTOM_NODE_DIR" ]; then CUSTOM_NODE_DIR=$CUSTOM_NODE_DIR; else CUSTOM_NODE_DIR="/tmp/node_modules";fi
+
+if [[ -z "$CUSTOM_NODE_DIR" ]]; then
+    if [ ! -d "$CUSTOM_NODE_DIR" ] ; then echo "$CUSTOM_NODE_DIR is not a folder";fi
+
+    echo "Please provide modules-folder . Usage export CUSTOM_NODE_DIR = xxx; myarn ";
+else
+    PATH=$CUSTOM_NODE_DIR/.bin:$PATH  NODE_PATH=$CUSTOM_NODE_DIR:$NODE_PATH yarn  --modules-folder $CUSTOM_NODE_DIR $@
+fi
+}
+
+EOF
