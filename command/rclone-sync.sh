5695c549403d3d148af3c674f514c95794b26d09
waxz <waxz@github.com>
2025-05-18 14:15:32 +0000

    change to r2 drive

--- command/rclone-sync.sh
+++ command/rclone-sync.sh
@@ -13,9 +13,19 @@ DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
 
 WATCH_DIR="/tmp/code"
 mkdir -p $WATCH_DIR
-REMOTE="onedrive:mybackup"
+REMOTE="r2:r2disk/code"
 FILTER="$DIR/rclone-filter.txt"
 LOG="/tmp/rclone-sync.log"
+
+local_folder_size=$(du -s "$WATCH_DIR" | awk '{print $1}')
+echo "Local folder size: $local_folder_size"
+remote_folder_size=$(rclone size "$REMOTE" --json | jq -r '.bytes')
+echo "Remote folder size: $remote_folder_size"
+
+if [ $local_folder_size -gt 1000000000 ];then
+    echo "Local folder size is greater than 1GB"
+    exit 1;
+fi;
 
 if [ ! -z "$D" ];then
     echo  download
@@ -31,9 +41,17 @@ fi;
 
 
 if [ ! -z "$S" ];then
+    echo "Starting sync..." >> "$LOG"
     while inotifywait -r -e modify,create,delete,move "$WATCH_DIR"; do
         echo "$(date): Change detected, syncing..." >> "$LOG"
+        local_folder_size=$(du -s "$WATCH_DIR" | awk '{print $1}')
+        if [ $local_folder_size -gt 1000000000 ];then
+            echo "Local folder size is greater than 1GB"
+            exit 0;
+        fi;
+        echo "Folder size: $local_folder_size" >> "$LOG"
         rclone sync "$WATCH_DIR" "$REMOTE"   -M  --filter-from "$FILTER" >> "$LOG" 2>&1
+        echo "Sync completed" >> "$LOG"
     done
 fi;
 
