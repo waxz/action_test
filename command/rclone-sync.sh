5e87277b2cb217b9a58e8e8a53149925e4d3d7e7
waxz <waxz@github.com>
2025-06-03 08:25:06 +0000

    update rclone

--- command/rclone-sync.sh
+++ command/rclone-sync.sh
@@ -18,6 +18,7 @@ FILTER="$DIR/rclone-filter.txt"
 LOG="/tmp/rclone-sync.log"
 
 local_folder_size=$(du -s "$WATCH_DIR" | awk '{print $1}')
+local_folder_size=$(rclone size "$WATCH_DIR" --json --filter-from "$FILTER"  | jq -r ".bytes")
 echo "Local folder size: $local_folder_size"
 remote_folder_size=$(rclone size "$REMOTE" --json | jq -r '.bytes')
 echo "Remote folder size: $remote_folder_size"
@@ -41,17 +42,17 @@ fi;
 
 
 if [ ! -z "$S" ];then
-    echo "Starting sync..." >> "$LOG"
-    while inotifywait -r -e modify,create,delete,move "$WATCH_DIR"; do
-        echo "$(date): Change detected, syncing..." >> "$LOG"
-        local_folder_size=$(du -s "$WATCH_DIR" | awk '{print $1}')
+    echo "Starting sync..."
+    while inotifywait  -r -e modify,create,delete,move "$WATCH_DIR"; do
+        echo "$(date): Change detected, syncing..."
+        local_folder_size=$(rclone size "$WATCH_DIR" --json --filter-from "$FILTER"  | jq -r ".bytes")
         if [ $local_folder_size -gt 1000000000 ];then
             echo "Local folder size is greater than 1GB"
             exit 0;
         fi;
-        echo "Folder size: $local_folder_size" >> "$LOG"
-        rclone sync --skip-links "$WATCH_DIR" "$REMOTE"   -M  --filter-from "$FILTER" >> "$LOG" 2>&1
-        echo "Sync completed" >> "$LOG"
+        echo "Folder size: $local_folder_size"
+        rclone sync --skip-links "$WATCH_DIR" "$REMOTE"   -M  --filter-from "$FILTER" -P -v --ignore-checksum --size-only
+        echo "Sync completed"
     done
 fi;
 
