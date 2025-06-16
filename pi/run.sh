64bd1ae0a24d663c2c31e9b3de15db6603193bf8
wonder <waxz@users.noreply.github.com>
2025-06-17 02:01:02 +0800

    Update run.sh

--- pi/run.sh
+++ pi/run.sh
@@ -3,4 +3,5 @@ sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
 echo DNS=127.0.0.1 | sudo tee -a  /etc/systemd/resolved.conf
 sudo systemctl restart systemd-resolved
-docker compose up -d
+docker compose up
+#-d
